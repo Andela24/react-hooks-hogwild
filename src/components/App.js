a993ddac120d961429c8988a3ce410d31cbf293d
Andela Stevanovic <andjela.s166@gmail.com>
2022-02-15 09:12:27 -0500

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,18 @@
-import React from "react";
+import useState from "react";
 import Nav from "./Nav";
 
 import hogs from "../porkers_data";
+import HogContainer from "./HogContainer";
 
 function App() {
+console.log(hogs)
+
+
+
 	return (
 		<div className="App">
 			<Nav />
+			<HogContainer />
 		</div>
 	);
 }
